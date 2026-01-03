dbdcb87cc878465873b69dc071bbc8929e1f13c1
thisisfaizanali <faizanaliwhb@gmail.com>
2026-01-03 13:34:42 +0530

    feat : add auth using Clerk

--- schemas/signUpSchema.ts
+++ schemas/signUpSchema.ts
@@ -1,15 +1,20 @@
 import * as z from 'zod';
 
-export const signUpSchema = z.object({
-  email: z
-    .string()
-    .min(1, { message: 'Email is required' })
-    .email({ message: 'Please enter a valid email' }),
-  password: z
-    .string()
-    .min(1, { message: 'Password is required' })
-    .min(1, { message: 'Password should be minimum 8 chars long' }),
-  passwordConfirmation: z
-    .string()
-    .min(1, { message: 'Please confirm your password' }),
-});
+export const signUpSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, { message: 'Email is required' })
+      .email({ message: 'Please enter a valid email' }),
+    password: z
+      .string()
+      .min(1, { message: 'Password is required' })
+      .min(8, { message: 'Password should be minimum 8 chars long' }),
+    passwordConfirmation: z
+      .string()
+      .min(1, { message: 'Please confirm your password' }),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: 'Passwords do not match',
+    path: ['passwordConfirmation'],
+  });
